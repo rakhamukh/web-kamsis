e9051c9ae54298e37065c565755b984675ba0908
M Rakha Mukhlianda <rmukhlianda@gmail.com>
2020-04-09 04:07:06 +0700

    Add PHP code

--- register-page.php
+++ register-page.php
@@ -1,18 +1,37 @@
+<?php 
+
+require 'function.php';
+
+if ( isset($_POST["register"])) { 
+  if (register($_POST) > 0) {
+      echo " 
+        <script>
+          alert('Success');
+          document.location.href = 'login-page.php';
+        </script>
+    ";
+  } else { 
+      echo "
+        <script>
+          alert('Failed!');
+        </script>
+      ";
+  }
+}
+
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
     <title>Simple Blog</title>
   </head>
-
-
-  <body>
+    <body>
     <div class="sidenav">
          <div class="login-main-text">
             <h2>Simple Blog</h2>
@@ -22,31 +41,34 @@
         <div class="main">
           <div class="col-md-6 col-sm-12">
             <div class="login-form">
-              <form method="POST">
+              <form action="" method="POST">
+                  <div class="form-group">
+                    <label>Nama Lengkap</label>
+                    <input type="text" class="form-control" placeholder="Nama Lengkap" name="nama" required>
+                  </div>
                   <div class="form-group">
                     <label>Username</label>
-                    <input type="text" class="form-control" placeholder="Username" name="username">
+                    <input type="text" class="form-control" placeholder="Username" name="username" required>
                   </div>
-                  <div>
-                    <input type="email" name="email" class="form-control" placeholder="E-mail">
-                    <label for="email">Remember me</label>
+                  <div calss="form-group">
+                    <label>Email</label>
+                    <input type="email" class="form-control" placeholder="E-mail" name="email" required>
                   </div>
                   <div class="form-group">
                     <label>Password</label>
-                    <input type="password" class="form-control" placeholder="Password" name="password">
+                    <input type="password" class="form-control" placeholder="Password" name="password" required>
                   </div>
                   <div class="form-group">
                     <label>Re-Password</label>
-                    <input type="password" class="form-control" placeholder="Re-Password" name="re-password">
+                    <input type="password" class="form-control" placeholder="Re-Password" name="re-password" required>
                   </div>
-                  <button type="submit" class="btn btn-secondary">Register</button>
+                  <button type="submit" name="register"class="btn btn-secondary">Register</button>
               </form>
             </div>
           </div>
         </div>
       </div>
     </div>
-
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
