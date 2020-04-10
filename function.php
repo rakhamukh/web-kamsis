71bb7bb7d108fd218f6359d72b07c401060bd124
M Rakha Mukhlianda <rmukhlianda@gmail.com>
2020-04-10 20:12:52 +0700

    Fix function

--- function.php
+++ function.php
@@ -1,24 +1,32 @@
 <?php 
   $db = mysqli_connect("localhost", "root", "", "webkamsis");
 
+  function query($query) {
+  	global $db;
+  	$result = mysqli_query($id, $query);
+  	$rows = [];
+  	while ( $row = mysqli_fetch_assoc($result) ) {
+  		$rows[] = $row;
+  	}
+  	return $rows;
+  }
 
   function register($datauser) {
-  global $db;
+  	global $db;
 
-  $nama = htmlspecialchars($datauser["nama"]);
+  	$nama = htmlspecialchars($datauser["nama"]);
 	$username = strtolower(stripcslashes($datauser["username"]));
 	$email = htmlspecialchars($datauser["email"]);
 	$password = mysqli_real_escape_string($db,$datauser["password"]);
-	$repassword = mysqli_real_escape_string($db,$datauser["password"]);
+	$repassword = mysqli_real_escape_string($db,$datauser["repassword"]);
 
-	$result = mysqli_query($db, "SELECT username FROM user WHERE username = '$username");
+	$result = mysqli_query($db, "SELECT username FROM user WHERE username = '$username' ");
 
 	if (mysqli_fetch_assoc($result)) {
 		echo "
 			<script>
 				alert('Username already use')
-			<script>
-		";
+			<script>";
 		return false;
 	}
 
@@ -26,17 +34,13 @@
 		echo "
 			<script>
 				alert('Password did not match')
-			<script>
-		";
+			<script>";
 		return false;
 	}
 
 	$password = password_hash($password, PASSWORD_DEFAULT);
 
-	$query = "INSERT INTO user VALUES
-            ('$nama', '$username', '$email', '$password')
-            ";
-  	mysqli_query($db, $query);
+  	mysqli_query( $db, " INSERT INTO user VALUES ('','$nama', '$username', '$email', '$password') " );
 
   	return mysqli_affected_rows($db);
   }
